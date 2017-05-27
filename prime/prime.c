9d5b687ad5b01931ac6a758fc7675d0a9ff83176
rahul357 <rahul40800@gmail.com>
2017-05-27 07:48:29 +0530

    command-line arguments

--- prime/prime.c
+++ prime/prime.c
@@ -2,9 +2,12 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 
+static int n = 5;
+module_param(n,int,0);
+MODULE_PARM_DESC(n,"given number");
+
 int init_module(void){
 
-	static int n = 8;
 	static int i =1;
 	static int prime =0;
 	for( i=2; i<=n/2;i++)if(n%i==0){
@@ -12,7 +15,7 @@ int init_module(void){
 	    break;}
 
 
-	if(prime)printk(KERN_INFO "Number is Prime");
+	if(!prime)printk(KERN_INFO "Number is Prime");
 	else printk(KERN_INFO "NOT PRIME");
     
     return 0;   
